7b68ddbceae72fcb4829c7fb1ae3884e5d98fdbd
Eduardo Pinheiro Miranda <pinheiomiranda@gmail.com>
2025-05-21 20:12:41 -0300

    esqueci de mandar os arquivos

---
+++ app-financas/src/components/balance/index.js
@@ -0,0 +1,35 @@
+import React, { useMemo } from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+
+export function Balance({ data }){
+
+    const labelName = useMemo(() => {
+        if(data.tag === "saldo"){
+            return{
+                label: "Entradas de hoje",
+                color: "#3B3DBF",
+            }
+        }
+        if(data.tag === "receita"){
+            return{
+                label: "Entradas de hoje",
+                color: "#00B94A",
+            }
+        }
+        if(data.tag === "despesa"){
+            return{
+                label: "Entradas de hoje",
+                color: "#EF463A",
+            }
+        }
+    }, [data]);
+
+    return(
+        <View style={[styles.container, {backgroundColor: labelName.color}]}>
+            <Text style={styles.label}>{labelName.label}</Text>
+            <Text style={styles.saldo}>R$ {data.saldo}</Text>
+        </View>
+    );
+}
---
+++ app-financas/src/components/balance/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        height: 150,
+        width: 300,
+        borderRadius: 10,
+        justifyContent: "center",
+        gap: 15,
+        paddingLeft: 25,
+        marginHorizontal: 15
+    },
+    label: {
+        fontSize: 18,
+        color: "#FFFFFF"
+    },
+    saldo: {
+        fontSize: 30,
+        color: "#FFFFFF"
+    }
+})
