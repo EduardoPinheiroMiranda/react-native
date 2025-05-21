53cd0c544e804ed02732502f91d6692f449eadf3
Eduardo Pinheiro Miranda <pinheiomiranda@gmail.com>
2025-05-21 20:12:06 -0300

    novo componente

--- app-financas/src/pages/home/index.js
+++ app-financas/src/pages/home/index.js
@@ -1,19 +1,21 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text, Button, SafeAreaView } from "react-native";
+import { View, Text, Button, SafeAreaView, FlatList } from "react-native";
 import { styles } from "./styles";
 import { HOST } from "@env";
-import { AuthContext } from "../../contexts/auth"
+import { AuthContext } from "../../contexts/auth";
+import { Balance } from "../../components/balance";
 
 
 import { Header } from "../../components/header";
 import { format } from "date-fns";
+import { useIsFocused } from "@react-navigation/native";
 
 
 export function Home(){
     
+    const useFocused = useIsFocused();
     const { token } = useContext(AuthContext);
     const [listBalance, setListBalance] = useState([]);
-    const [dateMovements, setDateMovements] = useState(new Date());
 
 
     useEffect(() => {
@@ -24,7 +26,7 @@ export function Home(){
 
             try{
 
-                const dateFormated = format(dateMovements, "dd/mm/yyyy");
+                const dateFormated = format(new Date(), "dd/mm/yyyy");
                 const request = await fetch(`${HOST}/balance?date=${dateFormated}`, {
                     method: "GET",
                     headers: {
@@ -38,22 +40,30 @@ export function Home(){
                     setListBalance(data);
                 }
 
-
-                console.log(listBalance);
             }catch(err){
                 console.log(err);
             }           
         }
 
         getMovements();
+        
 
         return () => isActive = false;
     }, []);
 
+    console.log(listBalance)
 
     return(
         <SafeAreaView>
             <Header title="Minhas movimentações"/>
+
+            <FlatList
+                data = {listBalance}
+                renderItem = {({item}) => <Balance data={item}/>}
+                horizontal = {true}
+                showsHorizontalScrollIndicator = {false}
+                keyExtractor = {item => item.tag}
+            />
         </SafeAreaView>
     );
 }
