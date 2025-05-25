b11982224c8f2a36b0de482d496899ced993b343
Eduardo Pinheiro Miranda <pinheiomiranda@gmail.com>
2025-05-25 18:52:56 -0300

    criando projeto para estudo

---
+++ notificacao/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aqui vou aprender a usar notificações</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
