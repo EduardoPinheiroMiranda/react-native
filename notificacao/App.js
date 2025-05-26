0ea522738e101c7d58c010870a990dbc87e2229d
Eduardo Pinheiro Miranda <pinheiomiranda@gmail.com>
2025-05-26 16:18:24 -0300

    estudadndo praticas de notificações

--- notificacao/App.js
+++ notificacao/App.js
@@ -1,11 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notification from "expo-notifications";
+
+Notification.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true, // Mostra o banner (popup superior)
+    shouldShowList: true,   // Mostra na lista de notificações (Notification Center)
+  })
+})
+
 
 export default function App() {
+
+  async function handlerNotification(){
+
+    try{
+
+      const { status } = await Notification.getPermissionsAsync();
+      if(status !== "granted"){
+        alert("Acesso negado.");
+        return;
+      }
+
+      let expoToken = await Notification.getExpoPushTokenAsync();
+      console.log(expoToken);
+      
+      await Notification.scheduleNotificationAsync({
+        content: {
+          title: "teste de msg",
+          body: "esta menssagem esta sendo enviada!",
+          data: {}
+        },
+        trigger: {
+          second: 30,
+        }
+      })
+
+
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Aqui vou aprender a usar notificações</Text>
-      <StatusBar style="auto" />
+
+      <Button 
+        onPress={handlerNotification}
+        title="dispara notificação"
+      />
     </View>
   );
 }
